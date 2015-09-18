44ea82589ef04d1049df9bd6594bb2c33a0585f4
olgamelnichuk <olkin@ebi.ac.uk>
2015-09-18 14:46:21 +0100

    added [scheduler] prefix to the log messages from JobScheduler

--- ngspyeasy/job_scheduler.py
+++ ngspyeasy/job_scheduler.py
@@ -18,16 +18,16 @@ class JobScheduler(Thread):
     def __init__(self, logfile=None, timeout=60):
         super(JobScheduler, self).__init__()
         self.logger = self.create_logger(logfile)
-        self.logger.debug("job_scheduler_init")
+        self.logger.debug("[scheduler]: job_scheduler_init")
 
         numcores = multiprocessing.cpu_count()  # min=8
-        self.logger.debug("numcores=%d", numcores)
+        self.logger.debug("[scheduler]: numcores=%d", numcores)
 
         if numcores < 2:
             raise RuntimeError("Number of available cores %d (< 2).", numcores)
 
         numjobsallowed = numcores / 2
-        self.logger.debug("numjobsallowed=%d", numjobsallowed)
+        self.logger.debug("[scheduler]: numjobsallowed=%d", numjobsallowed)
 
         self.processes = []
         self.tree = JobDependencyTree()
@@ -38,7 +38,7 @@ class JobScheduler(Thread):
     def create_logger(self, logfile):
         logger = logging.getLogger("JobScheduler")
         logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s %(threadName)s %(levelname)s [JobScheduler] %(message)s')
+        formatter = logging.Formatter('%(asctime)s %(threadName)s %(levelname)s %(message)s')
 
         if logfile:
             file_handler = logging.FileHandler(logfile, mode='w')
@@ -57,18 +57,18 @@ class JobScheduler(Thread):
             while not job_requests.empty():
                 (req_id, req_dep, req_command) = job_requests.get()
                 job_requests.task_done()
-                self.logger.debug("job_request_found: %s", req_id)
+                self.logger.debug("[scheduler]: job_request_found: %s", req_id)
 
                 if req_id == "stop_all":
-                    self.logger.info("[stop_all] message received. Preparing to stop...")
+                    self.logger.info("[scheduler]: (stop_all) message received. Preparing to stop...")
                     self.stopped = True
                     break
 
                 try:
                     self.tree.append(req_id, req_dep, req_command)
-                except ValueError as ex:
-                    self.logger.error(ex.message, ex.args)
-                    self.logger.info("Preparing to stop...")
+                except ValueError, e:
+                    self.logger.exception(e)
+                    self.logger.info("[scheduler]: Preparing to stop...")
                     self.stopped = True
                     break
 
@@ -78,15 +78,15 @@ class JobScheduler(Thread):
                     if job_id is not None:
                         self.tree.done(job_id, 0)
                 else:
-                    self.logger.debug("job_to_run: %s", job_id)
-                    self.logger.debug("command_to_run: [[\n%s \n]]", job_command)
+                    self.logger.debug("[scheduler]: job_to_run: %s", job_id)
+                    self.logger.debug("[scheduler]: command_to_run: [[\n%s \n]]", job_command)
 
                     proc = subprocess.Popen(shlex.split(job_command), shell=True)
                     self.processes.append((proc, job_command, job_id))
 
             self.update_processes()
 
-        self.logger.info("stopping all running processes..")
+        self.logger.info("[scheduler]: stopping all running processes..")
 
         waiting_time = 0
         while len(self.processes) > 0:
@@ -99,7 +99,7 @@ class JobScheduler(Thread):
                 self.update_processes()
             waiting_time += 0.5
 
-        self.logger.info("all stopped")
+        self.logger.info("[scheduler]: all stopped")
 
     def update_processes(self):
         unfinished = []
@@ -108,10 +108,10 @@ class JobScheduler(Thread):
             if ret is None:
                 unfinished.append((p, c, job_id))
             else:
-                self.logger.debug("job_done: %s", job_id)
+                self.logger.debug("[scheduler]: job_done: %s", job_id)
                 self.tree.done(job_id, ret)
                 if ret != 0:
-                    self.logger.error("Command [[\n%s \n]] completed with error. See logs for details", c)
+                    self.logger.error("[scheduler]: Command [[\n%s \n]] completed with error. See logs for details", c)
 
         self.processes = unfinished
 
@@ -121,7 +121,7 @@ class JobScheduler(Thread):
             ret = p.poll()
             if ret is None:
                 unfinished.append((p, c, job_id))
-                self.logger.error("Terminating process after timeout (%d) [[\n%s \n]]", self.timeout, c)
+                self.logger.error("[scheduler]: Terminating process after timeout (%d) [[\n%s \n]]", self.timeout, c)
                 if sigkill:
                     p.kill()
                 else:
