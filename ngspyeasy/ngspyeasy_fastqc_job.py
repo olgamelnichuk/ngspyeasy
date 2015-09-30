f15ef2bb8ac07e02bb623a86ad5286e79323f92b
olgamelnichuk <olkin@ebi.ac.uk>
2015-09-30 10:44:57 +0100

    experimenting with the subprocess shell

--- ngspyeasy/ngspyeasy_fastqc_job.py
+++ ngspyeasy/ngspyeasy_fastqc_job.py
@@ -127,13 +127,13 @@ def run_fastqc(row, projects_home):
            "--dir", get_sample_tmp_dir(sample_dir), "--outdir", get_sample_fastq_dir(sample_dir), fastq1, fastq2]
 
     try:
-        output = subprocess.check_output("env", stderr=subprocess.STDOUT, shell=True, executable='/bin/bash',
+        output = subprocess.check_output(["/bin/bash", "-c", "env"], stderr=subprocess.STDOUT, shell=True,
                                          env=os.environ.copy())
         log_info("env: \n" + output)
-        output = subprocess.check_output("echo $CLASSPATH", stderr=subprocess.STDOUT, shell=True, executable='/bin/bash',
+        output = subprocess.check_output(["/bin/bash", "-c", "echo $CLASSPATH"], stderr=subprocess.STDOUT, shell=True,
                                          env=os.environ.copy())
         log_info("echo $CLASSPATH: \n" + output)
-        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, executable='/bin/bash',
+        output = subprocess.check_output(["/bin/bash", "-c", cmd], stderr=subprocess.STDOUT, shell=True,
                                          env=os.environ.copy())
         retcode = 0
     except subprocess.CalledProcessError, ex:
