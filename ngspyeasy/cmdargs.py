53bd716b9a32521bfec1083a838bcc708bb52a85
olgamelnichuk <olkin@ebi.ac.uk>
2015-10-13 09:37:09 +0100

    removed unused code

--- ngspyeasy/cmdargs.py
+++ ngspyeasy/cmdargs.py
@@ -1,46 +1,8 @@
 #!/usr/bin/env python
+
 import argparse
 
 import os.path
-
-def check_cmdline_options(tsv_config_file, ngs_projects_dir):
-    (projects_home, errmsg) = check_ngs_projects_dir_option(ngs_projects_dir)
-    if errmsg is not None:
-        return None, None, errmsg
-
-    (tsv_name, errmsg) = check_tsv_config_file_option(tsv_config_file, projects_home)
-    if errmsg is not None:
-        return None, None, errmsg
-
-    return tsv_name, projects_home, None
-
-
-def check_ngs_projects_dir_option(ngs_projects_dir):
-    if not ngs_projects_dir:
-        return None, "Projects directory is a required parameter. Can't find one."
-
-    projects_home = os.path.abspath(ngs_projects_dir)
-
-    if not os.path.isdir(projects_home):
-        return None, "Projects directory '" + projects_home + "' does not exist."
-
-    return projects_home, None
-
-
-def check_tsv_config_file_option(tsv_config_file, projects_home):
-    if not tsv_config_file:
-        return None, None, "TSV config file is not specified."
-
-    expected_path = projects_home.config_path(os.path.basename(tsv_config_file))
-
-    if os.path.isabs(tsv_config_file):
-        if os.path.abspath(tsv_config_file) != expected_path:
-            return None, "Config file must be in the projects config directory: '%s'" % projects_home.config_dir()
-
-    if not os.path.isfile(expected_path):
-        return None, "Config file '" + expected_path + "' does not exist."
-
-    return os.path.basename(expected_path), None
 
 
 def existed_directory_path(path):
