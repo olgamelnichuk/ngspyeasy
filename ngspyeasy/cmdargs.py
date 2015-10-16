430812785040dd3c1d00f3842eace013f2fa3c8c
olgamelnichuk <olkin@ebi.ac.uk>
2015-10-16 14:16:48 +0100

    default value for task is no-task

--- ngspyeasy/cmdargs.py
+++ ngspyeasy/cmdargs.py
@@ -12,7 +12,7 @@ def parse_job_args(argv, name=""):
     parser.add_argument("-d", "--projects-dir", dest="projects_dir", required=True, type=existed_directory_path,
                         help="ngs_projects directory path")
     parser.add_argument("-i", "--sample_id", dest="sample_id", help="sample_id to run %s on" % name)
-    parser.add_argument("-t", "--task", dest="task", help="%s task")
+    parser.add_argument("-t", "--task", dest="task", default="no-task", help="%s task")
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="turn ON verbose mode")
     parser.add_argument("--version", action="version", version="%(prog)s 0.1", help="print software version")
     return parser.parse_args(argv)
