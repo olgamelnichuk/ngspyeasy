0a1a97c962fb7e781f86486a8c2e4d9a650c7ed9
olgamelnichuk <olkin@ebi.ac.uk>
2015-10-21 15:24:23 +0100

    bugfix: ValueError: Invalid placeholder in string: line 1, col 392

--- ngspyeasy/resources/vc/prepare/callable-regions.tmpl.sh
+++ ngspyeasy/resources/vc/prepare/callable-regions.tmpl.sh
@@ -9,8 +9,3 @@ time sambamba view \
 -t ${NCPU} -F "mapping_quality>1 and (paired or proper_pair) and (not unmapped)" \
 -f bam ${BAM_FILE} | \
 bedtools genomecov -split -ibam stdin -bga -max 500 | awk '\$4>1' > ${GENOMECOV_BED}
-
-# quick fix to get mapped regrions file for freebayes-parallel
-# START Make regions files for var-callers
-#awk '{print $1":"$2".."$3}' ${MAPPED_READS_BED} > ${MAPPED_REGIONS}
-#awk '{print $1":"$2"-"$3}'  ${MAPPED_READS_BED} > ${PLATYPUS_INTERVALS}
