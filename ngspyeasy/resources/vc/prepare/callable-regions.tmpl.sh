b315671cac7e895073894a09b6e3c50d5fedac0a
olgamelnichuk <olkin@ebi.ac.uk>
2015-10-21 15:40:53 +0100

    bugfix: ValueError: Invalid placeholder in string: line 1, col 392

--- ngspyeasy/resources/vc/prepare/callable-regions.tmpl.sh
+++ ngspyeasy/resources/vc/prepare/callable-regions.tmpl.sh
@@ -8,4 +8,4 @@ bedtools merge > ${MAPPED_READS_BED} && \
 time sambamba view \
 -t ${NCPU} -F "mapping_quality>1 and (paired or proper_pair) and (not unmapped)" \
 -f bam ${BAM_FILE} | \
-bedtools genomecov -split -ibam stdin -bga -max 500 | awk '\$4>1' > ${GENOMECOV_BED}
+bedtools genomecov -split -ibam stdin -bga -max 500 | awk '\$$4>1' > ${GENOMECOV_BED}
