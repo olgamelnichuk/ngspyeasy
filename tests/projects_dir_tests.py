065a1cadbfe9f204bbdffa484b0eafaae1bb8f76
olgamelnichuk <olkin@ebi.ac.uk>
2015-10-23 14:45:47 +0100

    tests updated

--- tests/projects_dir_tests.py
+++ tests/projects_dir_tests.py
@@ -11,25 +11,28 @@ from testutils import create_ngs_projects_dir, file_permissions
 
 class NGSProjectsStructureTest(unittest.TestCase):
     def test_directory_structure(self):
-        projects_home = create_ngs_projects_dir("test_directory_structure",
+        ngs_projects = create_ngs_projects_dir("test_directory_structure",
                                                 get_resource_path("ngspyeasy_test.config.tsv"))
-        tsv_conf = tsv_config.parse(projects_dir.config_full_path(projects_home, "ngspyeasy_test.config.tsv"))
-        projects_dir.init(projects_home, tsv_conf)
+
+        projects_home = projects_dir.ProjectsDir(ngs_projects)
+
+        tsv_conf = tsv_config.parse(projects_home.config_path("ngspyeasy_test.config.tsv"))
+        projects_home.init_structure(tsv_conf)
 
         row0 = tsv_conf.row_at(0)
-        self.assertTrue(isdir(projects_dir.project_dir(projects_home, row0.project_id())))
+        self.assertTrue(isdir(projects_home.project_dir(row0.project_id())))
 
-        sample_dir = projects_dir.sample_dir(projects_home, row0.project_id(), row0.sample_id())
-        self.assertTrue(isdir(sample_dir))
-        self.assertTrue(isdir(projects_dir.sample_tmp_dir(sample_dir)))
-        self.assertTrue(isdir(projects_dir.sample_config_dir(sample_dir)))
-        self.assertTrue(isdir(projects_dir.sample_log_dir(sample_dir)))
-        self.assertTrue(isdir(projects_dir.sample_fastq_dir(sample_dir)))
-        self.assertTrue(isdir(projects_dir.sample_alignments_dir(sample_dir)))
-        self.assertTrue(isdir(projects_dir.sample_reports_dir(sample_dir)))
-        self.assertTrue(isdir(projects_dir.sample_vcf_dir(sample_dir)))
+        sample_dir = projects_dir.SampleDir(projects_home.sample_dir(row0.project_id(), row0.sample_id()))
+        self.assertTrue(isdir(sample_dir.root()))
+        self.assertTrue(isdir(sample_dir.tmp_dir()))
+        self.assertTrue(isdir(sample_dir.config_dir()))
+        self.assertTrue(isdir(sample_dir.log_dir()))
+        self.assertTrue(isdir(sample_dir.fastq_dir()))
+        self.assertTrue(isdir(sample_dir.alignments_dir()))
+        self.assertTrue(isdir(sample_dir.reports_dir()))
+        self.assertTrue(isdir(sample_dir.vcf_dir()))
 
-        for root, dirs, files in os.walk(projects_home):
+        for root, dirs, files in os.walk(projects_home.root()):
             for name in files:
                 self.assertTrue(0775, file_permissions(os.path.join(root, name)))
 
@@ -37,29 +40,33 @@ class NGSProjectsStructureTest(unittest.TestCase):
                 self.assertTrue(0775, file_permissions(os.path.join(root, name)))
 
     def test_fastq_not_exist(self):
-        projects_home = create_ngs_projects_dir("test_fastq_not_exist",
+        ngs_projects = create_ngs_projects_dir("test_fastq_not_exist",
                                                 get_resource_path("ngspyeasy_test.config.tsv"))
-        tsv_conf = tsv_config.parse(projects_dir.config_full_path(projects_home, "ngspyeasy_test.config.tsv"))
-        projects_dir.init(projects_home, tsv_conf)
+        projects_home = projects_dir.ProjectsDir(ngs_projects)
+
+        tsv_conf = tsv_config.parse(projects_home.config_path("ngspyeasy_test.config.tsv"))
+        projects_home.init_structure(tsv_conf)
 
         try:
-            projects_dir.init_fastq(tsv_conf, projects_home)
+            projects_home.check_fastq(tsv_conf)
             self.fail("Exception should be raised, as fastq files do not exist")
         except IOError:
             pass
 
     def test_fastq_exist(self):
-        projects_home = create_ngs_projects_dir("test_fastq_exist",
+        ngs_projects = create_ngs_projects_dir("test_fastq_exist",
                                                 get_resource_path("ngspyeasy_test.config.tsv"))
-        tsv_conf = tsv_config.parse(projects_dir.config_full_path(projects_home, "ngspyeasy_test.config.tsv"))
-        projects_dir.init(projects_home, tsv_conf)
 
-        raw_fastq_dir = os.path.join(projects_home, "raw_fastq")
-        os.makedirs(raw_fastq_dir)
-        open(os.path.join(raw_fastq_dir, "illumina.100bp.pe.wex.150x_1.fastq.gz"),'a').close()
-        open(os.path.join(raw_fastq_dir, "illumina.100bp.pe.wex.150x_2.fastq.gz"),'a').close()
+        projects_home = projects_dir.ProjectsDir(ngs_projects)
+
+        tsv_conf = tsv_config.parse(projects_home.config_path("ngspyeasy_test.config.tsv"))
+        projects_home.init_structure(tsv_conf)
+
+        os.makedirs(projects_home.raw_fastq_dir())
+        open(projects_home.raw_fastq_path("illumina.100bp.pe.wex.150x_1.fastq.gz"), 'a').close()
+        open(projects_home.raw_fastq_path("illumina.100bp.pe.wex.150x_2.fastq.gz"), 'a').close()
 
         try:
-            projects_dir.init_fastq(tsv_conf, projects_home)
+            projects_home.check_fastq(tsv_conf)
         except IOError:
             self.fail("Exception should not be raised, as fastq files exist")
