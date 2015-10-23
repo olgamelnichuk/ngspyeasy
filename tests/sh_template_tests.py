173f74cf84f8808620b6f7d447f8d9f35c30b574
olgamelnichuk <olkin@ebi.ac.uk>
2015-10-23 15:44:46 +0100

    added sh_template_tests

---
+++ tests/sh_template_tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import unittest
+
+from ngspyeasy import sh_template
+
+
+class ShTemplateTest(unittest.TestCase):
+    def test_realn_bam_template(self):
+        tmpl = sh_template.load("realn", "bam-realn", "bam-realn.tmpl.sh")
+        vars = ["NCPU", "DUPEMARK_BED", "DUPEMARK_BAM", "CHROMS", "REFFASTA", "KNOWN_INDELS", "DUPEMARK_REALN_BAM",
+                "DUPEMARK_REALN_FLAGSTAT", "DUPEMARK_REALN_BED", "TMP_DIR"]
+        params = dict(zip(vars, vars))
+        tmpl.validate(**params)
+        print("\n".join(tmpl.source(**params)))
