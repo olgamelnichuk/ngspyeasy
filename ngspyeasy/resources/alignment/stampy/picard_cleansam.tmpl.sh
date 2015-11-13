e854624789c1f6a3e57de8f49db67729126f9810
olgamelnichuk <olkin@ebi.ac.uk>
2015-11-13 11:25:22 +0000

    bugfix: wrong tmp folder path

--- ngspyeasy/resources/alignment/stampy/picard_cleansam.tmpl.sh
+++ ngspyeasy/resources/alignment/stampy/picard_cleansam.tmpl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-time java -Xmx12g -Djava.io.tmpdir=${SOUTDocker}/tmp -jar /usr/local/pipeline/picardtools/${PICARD_VERSION}/picard.jar CleanSam \
+time java -Xmx12g -Djava.io.tmpdir=${TMP_DIR} -jar /usr/local/pipeline/picardtools/${PICARD_VERSION}/picard.jar CleanSam \
 TMP_DIR=${TMP_DIR} \
 CREATE_INDEX=FALSE \
 VALIDATION_STRINGENCY=SILENT \
