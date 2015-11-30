28e6acdb586aba1a642176a00ab0699e51bf5593
olgamelnichuk <olkin@ebi.ac.uk>
2015-11-30 11:40:20 +0000

    sbin -> bin

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ cp -R ./ngspyeasy/* ${DEST}
 
 echo "#!/usr/bin/env sh
 python ${DEST}/ngspyeasy.py \"\$@\"" > /usr/bin/ngspyeasy
-chmod a+x /usr/sbin/ngspyeasy
+chmod a+x /usr/bin/ngspyeasy
 
 echo "#!/usr/bin/env sh
 python ${DEST}/ngspyeasy_tool.py \"\$@\"" > /usr/bin/ngspyeasy_tool
-chmod a+x /usr/sbin/ngspyeasy_tool
+chmod a+x /usr/bin/ngspyeasy_tool
