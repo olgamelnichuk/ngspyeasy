0d70422efcd9b20f587bff61485d2dd702bc73e3
olgamelnichuk <olkin@ebi.ac.uk>
2015-11-30 11:33:46 +0000

    bugfix: symbol '$' not escaped

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ mkdir -p ${DEST}
 cp -R ./ngspyeasy/* ${DEST}
 
 echo "#!/usr/bin/env sh
-python ${DEST}/ngspyeasy.py \"$@\"" > /usr/bin/ngspyeasy
+python ${DEST}/ngspyeasy.py \"\$@\"" > /usr/bin/ngspyeasy
 chmod a+x /usr/sbin/ngspyeasy
 
 echo "#!/usr/bin/env sh
-python ${DEST}/ngspyeasy_tool.py \"$@\"" > /usr/bin/ngspyeasy_tool
+python ${DEST}/ngspyeasy_tool.py \"\$@\"" > /usr/bin/ngspyeasy_tool
 chmod a+x /usr/sbin/ngspyeasy_tool
