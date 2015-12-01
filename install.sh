9f320b41723c86c7dfba78ea308608b104edc81f
olgamelnichuk <olkin@ebi.ac.uk>
2015-12-01 10:07:04 +0000

    using /bin/bash as shebang for the executables as it easy to compile with shc

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ DEST=/opt/ngspyeasy
 mkdir -p ${DEST}
 cp -R ./ngspyeasy/* ${DEST}
 
-echo "#!/usr/bin/env sh
+echo "#!/bin/bash
 python ${DEST}/ngspyeasy.py \"\$@\"" > /usr/bin/ngspyeasy
 chmod a+x /usr/bin/ngspyeasy
 
-echo "#!/usr/bin/env sh
+echo "#!/bin/bash
 python ${DEST}/ngspyeasy_tool.py \"\$@\"" > /usr/bin/ngspyeasy_tool
 chmod a+x /usr/bin/ngspyeasy_tool
