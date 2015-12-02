42534782c677848fd6758ee79953ed0eb5c2f756
olgamelnichuk <olkin@ebi.ac.uk>
2015-12-02 11:04:37 +0000

    brought back proper shebang

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ DEST=/opt/ngspyeasy
 mkdir -p ${DEST}
 cp -R ./ngspyeasy/* ${DEST}
 
-echo "#!/bin/bash
+echo "##!/usr/bin/env bash
 python ${DEST}/ngspyeasy.py \"\$@\"" > /usr/bin/ngspyeasy
 chmod a+x /usr/bin/ngspyeasy
 
-echo "#!/bin/bash
+echo "#!/usr/bin/env bash
 python ${DEST}/ngspyeasy_tool.py \"\$@\"" > /usr/bin/ngspyeasy_tool
 chmod a+x /usr/bin/ngspyeasy_tool
