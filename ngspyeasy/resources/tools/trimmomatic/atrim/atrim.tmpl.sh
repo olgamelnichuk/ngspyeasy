0501fa660d325682a4c129f6b9c47c8b2f484c71
olgamelnichuk <olkin@ebi.ac.uk>
2015-12-02 15:59:35 +0000

    bugfix: making trimmomatic jar accessible by users other than pipeman

--- ngspyeasy/resources/tools/trimmomatic/atrim/atrim.tmpl.sh
+++ ngspyeasy/resources/tools/trimmomatic/atrim/atrim.tmpl.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-java -XX:ParallelGCThreads=1 -jar /usr/local/pipeline/Trimmomatic-0.32/trimmomatic-0.32.jar PE \
+wget -O /tmp/Trimmomatic-0.32.zip http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.32.zip
+
+unzip /tmp/Trimmomatic-0.32.zip -d /tmp
+
+java -XX:ParallelGCThreads=1 -jar /tmp/Trimmomatic-0.32/trimmomatic-0.32.jar PE \
 -threads ${NCPU} \
 ${FQ1} ${FQ2} \
 ${PAIRED_FQ1} ${UNPAIRED_FQ1} \
